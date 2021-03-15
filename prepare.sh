bf0bb81db04ab1354e1164720ba4ba1b7852bcf1
No13 <github@frotmail.nl>
2021-03-15 20:07:55 +0000

    Pretty prepare :P

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-iw wlan0 set power_save off
-apt update
-apt install python3-pip pigpio python3-smbus i2c-tools
-pip3 install bme280pi RPi.gpio pigpio requests
-raspi-config nonint do_i2c 0
+TERM=ansi
+whiptail --infobox "WiFi powersave disabling..." 10 50
+iw wlan0 set power_save off 1> /dev/null
+whiptail --infobox "Updating APT" 10 50
+apt update 1> /dev/null
+whiptail --infobox "Installing APT packages" 10 50
+apt install python3-pip pigpio python3-smbus i2c-tools 1> /dev/null
+whiptail --infobox "Installing python3 packages" 10 50
+pip3 install bme280pi RPi.gpio pigpio requests 1> /dev/null
+whiptail --infobox "Enabling i2c" 10 50
+raspi-config nonint do_i2c 0 1> /dev/null
+echo "Done"
