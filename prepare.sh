95ede3f532cb1548fae7f25b722d4966b2c22521
Eric van Zandvoort <github@frotmail.nl>
2021-03-15 20:20:36 +0100

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 iw wlan0 set power_save off
 apt update
-apt install python3-pip pigpio python3-smbus
+apt install python3-pip pigpio python3-smbus i2c-tools
 pip3 install bme280pi RPi.gpio pigpio requests
